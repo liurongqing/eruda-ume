6db90418e3c4fbfb00674b4d6aead964f123c1d0
liurongqing <phpgege@gmail.com>
2022-06-07 14:07:06 +0800

    gohome

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import { ErudaUme } from './eruda-ume'
+import { ErudaUme } from './eruda-ume';
 
 if (window.eruda) {
   window.eruda.init({
-    tool: ['console'],
-  })
-  window.eruda.add(new ErudaUme())
+    tool: ['console', 'info'],
+  });
+
+  const erudaUme = new ErudaUme();
+  erudaUme.addAction([{ key: 'token', value: 'xxx' }]);
+  erudaUme.addAction({ key: 'aa', value: 'xxx2' });
+  window.eruda.add(erudaUme);
+
+  // const settings = window.eruda.get('settings');
+  // let cfg = eruda.Settings.createCfg('test');
+  // console.log('cfg', cfg);
+
+  // cfg.set({
+  //   testBool: true,
+  //   testSelect: 'select1',
+  //   testRange: 1,
+  // });
+
+  // settings.text('Test')
+  //         .switch(cfg, 'testBool', 'Test Bool')
+  //         .select(cfg, 'testSelect', 'Test Select', ['select1', 'select2'])
+  //         .range(cfg, 'testRange', 'Test Range', {min: 0, max: 1, step: 0.1})
+  //         .separator();
+
+  // settings.remove(cfg, 'testBool')
+
+  window.eruda.show();
+  window.eruda.show('info');
 }
